2dfb68578d9897b8354eb343bd948a0e300ada0b
Maurizio <maurizio.faedda1@gmail.com>
2020-12-02 11:37:10 +0100

    icon-social and footer menu with vuejs

---
+++ main.js
@@ -0,0 +1,36 @@
+var app = new Vue ({
+    el: '#root',
+    data: {
+        logos: [
+            'facebook-f',
+            'twitter',
+            'instagram',
+            'youtube',
+            'linkedin-in',
+            'tiktok'
+
+        ],
+        footer_menu: {
+            service: [
+                'Marketing Plan',
+                'Sales Development',
+                'Digital Marketing',
+                'Piercing',
+                'Why Us',
+                'Case Studies'
+
+            ],
+            resources:[
+                'Learning Center',
+                'Video Tutorials',
+                'Customers',
+                'Blog'
+            ],
+            company: [
+                'Who We Are',
+                'Contact Us',
+                'Careers'
+            ]
+        }
+    }
+})
